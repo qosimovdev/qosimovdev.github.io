f61aac472580c3f22aaa2257b4d89b09ba53e263
ibrohim3 <ibrohim.2128382@gmail.com>
2026-01-31 00:02:21 +0500

    Initial portfolio commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>👋 Hi there, I'm Ibrohim</h1>
+    <h2>💻 Full-Stack Creator from Uzbekistan 🇺🇿</h2>
+    <a href="https://t.me/qosimovdev">My Telegram</a>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+h1 {
+  font-size: 45px;
+}
+h2 {
+  font-size: 35px;
+}
+a {
+  font-size: 55px;
+}
+</style>
